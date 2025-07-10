a9369b2fb8e911f709a8c83581d92a3317fa4842
JAGT1806 <jagt1806@gmail.com>
2025-07-09 21:12:37 -0500

    first commit

---
+++ src/main/java/com/jagt/reader/mangareadermodule/MangaReaderModuleApplication.java
@@ -0,0 +1,13 @@
+package com.jagt.reader.mangareadermodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaReaderModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaReaderModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jagt/reader/mangareadermodule/MangaReaderModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jagt.reader.mangareadermodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaReaderModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
